9e1c4fd188aec6bede7dc8fb283f8ca5c196f360
eds9nsilva <antonioedson99@gmail.com>
2021-12-22 21:31:41 -0300

    drop pack

--- src/router.js
+++ src/router.js
@@ -1,2 +1,14 @@
 import React from "react";
-import {BrowserRouter, Route, Switch, Router} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from "./pages/home";
+
+
+export default function Routes(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' exact component={Home}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}

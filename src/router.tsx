9a4a044455cd9a60a9cbea2fa3b50564bebb6a25
eds9nsilva <antonioedson99@gmail.com>
2022-01-08 19:57:47 -0300

    router

---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import HomeIndex from "./pages/home";
+
+export default function RoutesPag(){
+    return(
+
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<HomeIndex/>} />
+
+            </Routes>
+        </BrowserRouter>
+
+
+    )
+}

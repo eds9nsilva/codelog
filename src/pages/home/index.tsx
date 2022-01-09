7ff345d9699da53d9090b6f3355e199564692f16
eds9nsilva <antonioedson99@gmail.com>
2022-01-08 23:56:21 -0300

    index

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './componets/navbar';
 import Header from './componets/header';
 import FeatureIndex from './componets/feature';
+import QuemSomos from './componets/quemSomos';
 export default function HomeIndex(){
     return(
         <>
             <Navbar/>
             <Header/>
             <FeatureIndex/>
+            <QuemSomos/>
         </>
     );
 }

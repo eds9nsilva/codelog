9d9a1c63ab48fcb9d993d2a6e58cb4f2f5a6be12
eds9nsilva <antonioedson99@gmail.com>
2022-01-08 23:56:13 -0300

    feature

--- src/pages/home/componets/feature.tsx
+++ src/pages/home/componets/feature.tsx
@@ -1,13 +1,18 @@
-import { Box, SimpleGrid } from '@chakra-ui/react';
+import { Box, SimpleGrid, Text } from '@chakra-ui/react';
 import * as React from 'react'
 import { FaNode, FaReact } from "react-icons/fa";
 import { SiAdobephotoshop, SiTypescript, SiExpress, SiChakraui } from "react-icons/si";
 import { Feature } from './featureIndex'
 
+
 export default function featureIndex(){
   return(
     <Box as="section" maxW="5xl" mx="auto" py="12" px={{ base: '6', md: '8' }}>
+      
+      <Text fontSize='4xl' align={'center'}>Algumas das Tecs que utilizamos</Text>
+      <br></br>
     <SimpleGrid columns={{ base: 1, md: 2 }} spacingX="10" spacingY={{ base: '8', md: '14' }}>
+             
       <Feature title="NodeJs" icon={<FaNode />}>
         At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
         takimata sanctus.
